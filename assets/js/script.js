1e7b0f164c397927702165c5e8ff4bef376205b6
toharimaolana <sitohari01@gmail.com>
2024-01-06 14:49:04 +0700

    improve

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const iconList = document.getElementById("icon-list");
+const menuList = document.getElementById("menu-list");
+
+
+
+iconList.addEventListener("click", () => {
+   menuList.classList.toggle("hidden")
+
+})
